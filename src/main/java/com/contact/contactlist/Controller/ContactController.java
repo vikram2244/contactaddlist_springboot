a4e3a0502d18cc02962c99f4bb8bdbaade6fa36a
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-03-11 23:18:24 +0530

    configure two

--- src/main/java/com/contact/contactlist/Controller/ContactController.java
+++ src/main/java/com/contact/contactlist/Controller/ContactController.java
@@ -23,7 +23,7 @@ import com.contact.contactlist.model.User;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "https://contactaddlist-reactjs.vercel.app/", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class ContactController {
 
 	@Autowired
