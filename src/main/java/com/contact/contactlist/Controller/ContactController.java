5bc86a77dfb66bf071514dbf28474fa3cc34e2f9
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-03-12 08:39:37 +0530

    updated

--- src/main/java/com/contact/contactlist/Controller/ContactController.java
+++ src/main/java/com/contact/contactlist/Controller/ContactController.java
@@ -23,7 +23,7 @@ import com.contact.contactlist.model.User;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class ContactController {
 
 	@Autowired
