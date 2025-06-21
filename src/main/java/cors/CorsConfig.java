03d31d417c5f5ce3690c3cb9dc11741d1bb25054
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-06-21 13:22:42 +0530

    Update CorsConfig.java

--- src/main/java/cors/CorsConfig.java
+++ src/main/java/cors/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
        registry.addMapping("/**")
-                .allowedOriginPatterns("https://contactsvikky.netlify.app")
+                .allowedOriginPatterns("https://newprojectcontact.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
