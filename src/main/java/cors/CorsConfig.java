84e7a7bc3804f1bdf3314f8058d03f422116063c
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-06-21 15:41:27 +0530

    Update CorsConfig.java

--- src/main/java/cors/CorsConfig.java
+++ src/main/java/cors/CorsConfig.java
@@ -12,7 +12,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
