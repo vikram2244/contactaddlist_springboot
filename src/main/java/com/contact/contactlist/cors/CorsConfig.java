6dd63b4d1ef04b74a937817f14f7bd75573ef717
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-06-21 21:25:37 +0530

    update new

--- src/main/java/com/contact/contactlist/cors/CorsConfig.java
+++ src/main/java/com/contact/contactlist/cors/CorsConfig.java
@@ -1,4 +1,4 @@
-package cors;
+package com.contact.contactlist.cors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
